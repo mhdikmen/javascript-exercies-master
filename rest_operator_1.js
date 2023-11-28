11f5757887643999ee29bbf01f215d0e58e627ec
Muhammed Hanifi Dikmen <muhadikmen@gmail.com>
2023-11-29 00:04:39 +0300

    rest  operator added

---
+++ rest_operator_1.js
@@ -0,0 +1,15 @@
+﻿const top7 = [
+  "The Colosseum",
+  "The Roman Forum",
+  "The Vatican",
+  "Trevi Fountain",
+  "The Pantheon",
+  "Piazza Venezia",
+  "The Palatine Hill",
+];
+const [] = top7;
+const [first, second, third, ...secondVisit] = top7;
+console.log(first);
+console.log(second);
+console.log(third);
+console.log(secondVisit);
